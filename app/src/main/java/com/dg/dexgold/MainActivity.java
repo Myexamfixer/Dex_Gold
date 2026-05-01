036db8cf84055f66feb54e37f1e610193ac9ab13
Myexamfixer <contact.myexamfixer@gmail.com>
2026-05-01 15:04:20 +0530

    Update MainActivity.java

--- app/src/main/java/com/dg/dexgold/MainActivity.java
+++ app/src/main/java/com/dg/dexgold/MainActivity.java
@@ -25,7 +25,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    String websiteURL = "https://dexgold.net/"; // sets web url
+    String websiteURL = "https://myexamfixer.blogspot.com/"; // sets web url
     private WebView webview;
     SwipeRefreshLayout mySwipeRefreshLayout;
 
